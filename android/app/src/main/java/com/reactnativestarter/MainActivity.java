20f597647d10b50103aa853efed39c420fac6544
Vlad <excelent91@mail.ru>
2021-02-18 22:45:53 +0200

    fixed error activity class does not exist for android build

--- android/app/src/main/java/com/reactnativestarter/MainActivity.java
+++ android/app/src/main/java/com/reactnativestarter/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativestarter;
+package com.reactnativestarter.upd;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/reactnativestarter/MainApplication.java
+++ android/app/src/main/java/com/reactnativestarter/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativestarter;
+package com.reactnativestarter.upd;
 
 import android.app.Application;
 import android.content.Context;

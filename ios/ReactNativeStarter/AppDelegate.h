c8b5bd7197f0c7f959da131d185f016b333a9445
baky30 <amanbakiev99@gmail.com>
2023-06-12 17:20:26 +0400

    Update ios Podfile

--- ios/ReactNativeStarter/AppDelegate.h
+++ ios/ReactNativeStarter/AppDelegate.h
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-#import <React/RCTAppDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : RCTAppDelegate
